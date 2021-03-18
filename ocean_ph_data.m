4b4af29f6f24e80dc1356a13c806464ef7557bb4
umangU <72771903+umangU@users.noreply.github.com>
2021-03-18 22:43:49 +1100

    Code File Uploaded

---
+++ ocean_ph_data.m
@@ -0,0 +1,20 @@
+clear
+clc
+
+fid = fopen('hots_data.txt','r');
+data = textscan(fid, '%d %D %f %f');
+fclose(fid);
+date = data{2};
+ph = data{3};
+pressure = data{4};
+subplot(2,1,1)
+%plot(date,ph)
+%xlabel('Date');
+%ylabel('pH Reading');
+
+
+min_depth = [0,10,25];
+max_depth = [10,25,50];
+[outDate1,outPH1,outDate2,outPH2,outDate3,outPH3] = oceandepth(date,ph,pressure,min_depth,max_depth);
+%subplot(2,1,2)
+%h1 = plot(outDate(1),outPH(1))
---
+++ oceandepth.m
@@ -0,0 +1,17 @@
+function [outDate1,outpH1,outDate2,outpH2,outDate3,outpH3] = oceandepth(date, ph, pressure,min_depth,max_depth)
+    for i=1:length(pressure)
+        if (pressure(i) >= min_depth(1) && pressure(i) < max_depth(1))
+            outDate1(i) = date(i);
+            outpH1(i) = ph(i);
+        elseif (pressure(i) >= min_depth(2) && pressure(i) < max_depth(2))
+            outDate2(i) = date(i);
+            outpH2(i) = ph(i);
+        elseif (pressure(i) >= min_depth(3) && pressure(i) < max_depth(3))
+            outDate3(i) = date(i);
+            outpH3(i) = ph(i);
+        else
+            continue
+        end
+    end
+end
+
