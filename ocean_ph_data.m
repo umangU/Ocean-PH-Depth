46b67cd58d877b58efecf02aea7b1015f03815f1
umangU <72771903+umangU@users.noreply.github.com>
2021-05-10 23:46:27 +1000

    Code Updated

--- ocean_ph_data.m
+++ ocean_ph_data.m
@@ -10,11 +10,11 @@ pressure = data{4};
 subplot(2,1,1)
 %plot(date,ph)
 %xlabel('Date');
-%ylabel('pH Reading');
+% ylabel('pH Reading');
 
 
 min_depth = [0,10,25];
 max_depth = [10,25,50];
 [outDate1,outPH1,outDate2,outPH2,outDate3,outPH3] = oceandepth(date,ph,pressure,min_depth,max_depth);
-%subplot(2,1,2)
-%h1 = plot(outDate(1),outPH(1))
+% subplot(2,1,2)
+% h1 = plot(outDate(1),outPH(1))
